cb824735391a1001245299cadc6b7a1aba3073a0
Dan Abs <danabsolonefl@gmail.com>
2023-02-21 11:28:21 +0000

    converted words to spans

--- public/script.js
+++ public/script.js
@@ -1,11 +1,33 @@
 const sourceTextContainer = document.querySelector('.source-text-container')
 
+const spanArray = []
+
+function populateSourceText(data) {
+  sourceTextContainer.innerHTML = ''
+  spanArray.splice(0, spanArray.length)
+  
+  data.forEach((text) => {
+    const p = document.createElement('p')
+    text.split(" ").forEach((word) => {
+      const span = document.createElement('span')
+      span.innerText = word
+      spanArray.push(span)
+      p.appendChild(span)
+      p.appendChild(document.createTextNode(" "))
+    })
+
+
+    sourceTextContainer.appendChild(p)
+
+  })
+
+}
+
 fetch('https://flipsum-ipsum.net/api/icw/v1/generate?ipsum=recipe-ipsum-text-generator&start_with_fixed=0&paragraphs=4')
   .then(response => response.json())
   .then((data) => {
-    data.forEach((text) => {
-      const p = document.createElement('p')
-      p.innerText = text
-      sourceTextContainer.appendChild(p)
-    })
+    populateSourceText(data)
   })
+
+
+
